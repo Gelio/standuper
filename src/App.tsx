14fb282c2704672651676e490146718cdc564be7
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-11-17 11:35:06 +0100

    fix: remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { createSignal } from "solid-js";
 import { Timer } from "./timer";
 
 function App() {
-  const [count, setCount] = createSignal(0);
-
   return (
     <div class="text-3xl m-10 flex flex-col justify-center">
       <Timer />
