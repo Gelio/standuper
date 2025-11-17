edb974544f380c01316fbad368cdc5dd418dec0a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-11-17 12:10:06 +0100

    test: more comprehensive tests for App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,53 @@
-import { expect, it } from "vitest";
-import { render } from "@solidjs/testing-library";
+import { afterEach, expect, test } from "vitest";
+import { page } from "vitest/browser";
+import { cleanup, render } from "@solidjs/testing-library";
 import App from "./App";
 
-it("renders a button", () => {
-  const { getByRole } = render(() => <App />);
+afterEach(() => {
+  cleanup();
+});
 
-  expect(getByRole("button")).toBeDefined();
+test("timer", async () => {
+  const { baseElement } = render(() => <App />);
+  const screen = page.elementLocator(baseElement);
+  const startButtonLocator = screen.getByRole("button", {
+    name: "Start Timer",
+  });
+
+  await expect.element(startButtonLocator).toBeVisible();
+  await startButtonLocator.click();
+
+  {
+    const pauseButtonLocator = screen.getByRole("button", {
+      name: "Pause Timer",
+    });
+    await expect.element(pauseButtonLocator).toBeVisible();
+  }
+
+  {
+    const resetButtonLocator = screen.getByRole("button", {
+      name: "Reset Timer",
+    });
+    await expect.element(resetButtonLocator).toBeVisible();
+    await expect.element(screen.getByText("0")).toBeVisible();
+    await resetButtonLocator.click();
+  }
+
+  await expect.element(startButtonLocator).toBeVisible();
+});
+
+test("find start button", async () => {
+  const { baseElement } = render(() => <App />);
+  const screen = page.elementLocator(baseElement);
+  const buttonLocator = screen.getByRole("button", { name: "Start Timer" });
+
+  await expect.element(buttonLocator).toBeVisible();
+});
+
+test("failing test", async () => {
+  const { baseElement } = render(() => <App />);
+  const screen = page.elementLocator(baseElement);
+  const buttonLocator = screen.getByRole("button", { name: "Resume Timer" });
+
+  await expect.element(buttonLocator).toBeVisible();
 });
