639b523cfa438f030c2ab93d69718057d3e4f405
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-11-24 10:46:04 +0100

    feat: add people list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { People } from "./people";
 import { Timer } from "./timer";
 
 function App() {
   return (
-    <div class="p-10">
+    <div class="p-10 flex flex-col gap-10">
       <Timer />
+      <People />
     </div>
   );
 }
---
+++ src/people.tsx
@@ -0,0 +1,111 @@
+import { For } from "solid-js";
+import { Box } from "./components/box";
+import { createStore, type SetStoreFunction } from "solid-js/store";
+import { Button } from "./components/button";
+
+type Person = {
+  element: HTMLInputElement | undefined;
+};
+
+export function People() {
+  const [people, setPeople] = createStore<Person[]>([{ element: undefined }]);
+
+  function shufflePeople() {
+    setPeople((p) => toShuffled(p));
+  }
+
+  return (
+    <Box>
+      <h2 class="text-2xl font-bold mb-2">People</h2>
+
+      <div class="flex flex-col gap-4">
+        <Button onclick={shufflePeople} class="self-start">
+          Shuffle
+        </Button>
+
+        <PeopleList people={people} setPeople={setPeople} />
+      </div>
+    </Box>
+  );
+}
+
+function toShuffled<T>(array: T[]): T[] {
+  const shuffled = array.slice();
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    // NOTE: use `i` instead of `i-1` to allow swapping with itself.
+    // This prevents situations where a 2-item array will always swap items.
+    const otherIndex = Math.floor(Math.random() * i);
+
+    const temp = shuffled[i];
+    shuffled[i] = shuffled[otherIndex];
+    shuffled[otherIndex] = temp;
+  }
+
+  return shuffled;
+}
+
+function PeopleList(props: {
+  people: Person[];
+  setPeople: SetStoreFunction<Person[]>;
+}) {
+  function insertPerson(index: number) {
+    props.setPeople((people) =>
+      people.toSpliced(index, 0, { element: undefined }),
+    );
+  }
+  function removePerson(index: number) {
+    props.setPeople((people) => people.toSpliced(index, 1));
+  }
+  function focusPersonNameInput(index: number) {
+    const person = props.people[index];
+    if (!person) {
+      return;
+    }
+    person.element?.focus();
+  }
+
+  return (
+    <ul class="list-disc pl-6 flex flex-col gap-2">
+      <For each={props.people}>
+        {(person, index) => (
+          <li>
+            <input
+              type="text"
+              class="w-full rounded border-b border-gray-300 focus:border-blue-500 focus:outline-none p-1 bg-blue-100"
+              ref={(element) => {
+                props.setPeople(index(), "element", element);
+              }}
+              onkeydown={(e) => {
+                if (e.key === "Enter") {
+                  e.preventDefault();
+                  const newRowIndex = index() + 1;
+                  insertPerson(newRowIndex);
+                  focusPersonNameInput(newRowIndex);
+                } else if (
+                  (e.key === "Backspace" || e.key === "Delete") &&
+                  person.element?.value === "" &&
+                  props.people.length > 1
+                ) {
+                  console.log("deleting");
+                  removePerson(index());
+                  focusPersonNameInput(Math.max(index() - 1, 0));
+                  e.preventDefault();
+                } else if (
+                  e.key === "ArrowDown" &&
+                  index() + 1 < props.people.length
+                ) {
+                  e.preventDefault();
+                  focusPersonNameInput(index() + 1);
+                } else if (e.key === "ArrowUp" && index() - 1 >= 0) {
+                  e.preventDefault();
+                  focusPersonNameInput(index() - 1);
+                }
+              }}
+            />
+          </li>
+        )}
+      </For>
+    </ul>
+  );
+}
