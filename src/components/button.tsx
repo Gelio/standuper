1a8987511320f214610aeb76757a1d83fe3b3b4a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-11-24 10:45:39 +0100

    style: add outline to buttons

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,6 +3,6 @@ import type { JSX } from "solid-js";
 export const Button = (props: JSX.ButtonHTMLAttributes<HTMLButtonElement>) => (
   <button
     {...props}
-    class={`bg-blue-500 text-white px-4 py-2 rounded ${props.class ?? ""}`}
+    class={`bg-blue-500 text-white px-4 py-2 rounded outline-offset-2 focus-visible:outline-sky-800 ${props.class ?? ""}`}
   />
 );
